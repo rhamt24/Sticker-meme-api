1e859c88e5fe37e2d0410c496a07d81ec723df8a
Zals <rahmataprizal403@gmail.com>
2024-07-17 18:41:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const { createCanvas, loadImage, registerFont } = require('canvas');
 const axios = require('axios');
 const path = require('path');
+const crypto = require('crypto');
 const app = express();
 const port = 3000;
 
 // Daftarkan font Bebas Neue
 registerFont(path.join(__dirname, 'fonts', 'BebasNeue-Bold.ttf'), { family: 'BebasNeue' });
+
+const images = new Map();
 
 async function createMeme(imageUrl, topText, bottomText) {
     const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
@@ -54,13 +57,28 @@ app.get('/meme', async (req, res) => {
 
     try {
         const memeBuffer = await createMeme(url, topText, bottomText);
-        res.set('Content-Type', 'image/png');
-        res.send(memeBuffer);
+        const memeId = crypto.randomBytes(16).toString('hex');
+        images.set(memeId, memeBuffer);
+
+        const memeUrl = `${req.protocol}://${req.get('host')}/image/${memeId}`;
+        res.json({ memeUrl });
     } catch (error) {
         res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+
+app.get('/image/:id', (req, res) => {
+    const memeBuffer = images.get(req.params.id);
+
+    if (memeBuffer) {
+        res.set('Content-Type', 'image/png');
+        res.send(memeBuffer);
+    } else {
+        res.status(404).json({ error: 'Image not found' });
     }
 });
 
 app.listen(port, () => {
     console.log(`Meme generator API listening at http://localhost:${port}`);
 });
+        
